6a04a6b51bc48b6aeb6073879c74d3df4b3d24c6
codeprogramAll <wachitoquevedo@yahoo.com>
2017-01-28 20:28:11 -0500

    Proyecto añadido
    se añade todo el proyecto al repositorio

---
+++ resources/views/usuario/forms/user.blade.php
@@ -0,0 +1,13 @@
+<div class="form-group">
+		{!! Form::label('Nombre') !!}
+		{!! Form::text('name',null,['class'=>'form-control','placeholder'=>'Ingrese el Nombre del usuario']) !!}
+	</div>
+	<div class="form-group">
+		{!! Form::label('Correo') !!}
+		{!! Form::text('email',null,['class'=>'form-control','placeholder'=>'Ingrese el correo del usuario']) !!}
+	</div>
+	<div class="form-group">
+		{!! Form::label('Contraseña') !!}
+		{!! Form::password('password',['class'=>'form-control','placeholder'=>'Ingrese la contraseña del usuario']) !!}
+	</div>
+	
